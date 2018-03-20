7a44dd729dc9b1c8c80bd96ac813c8a5a48f1335
Niels Weistra <niels.weistra@gmail.com>
2018-03-20 13:26:44 +0100

    Reorder choices

--- NO.PowershellDSC.Example.Web.ps1
+++ NO.PowershellDSC.Example.Web.ps1
@@ -18,7 +18,7 @@ function Main {
     $publish = new-Object System.Management.Automation.Host.ChoiceDescription "&Publish","help"
     $publishmods = new-Object System.Management.Automation.Host.ChoiceDescription "Publish &Modules","help"
     $abort = new-Object System.Management.Automation.Host.ChoiceDescription "&Abort","help"
-    $choices = [System.Management.Automation.Host.ChoiceDescription[]]($create,$deploy, $publish, $publishmods, $abort)
+    $choices = [System.Management.Automation.Host.ChoiceDescription[]]($deploy, $publish, $publishmods, $create, $abort)
     $answer = $host.ui.PromptForChoice($caption,$message,$choices,4)
     
     switch ($answer){
@@ -91,7 +91,6 @@ function Publish-Modules {
     Compress-Archive -Path $ModulePath -DestinationPath $DestinationPath
     New-DscChecksum -Path $DestinationPath
 }
-
 function Create-Configs {
     # Create MOFs
     NO.PowershellDSC.Example.Web -ConfigurationData .\ConfigData.psd1 -Verbose
@@ -107,21 +106,21 @@ Configuration NO.PowershellDSC.Example.Web
     Node $AllNodes.Where({$_.Roles -contains 'Web'}).NodeName
     {
         # Configure for web server role
-        WindowsFeature DotNet45Core
-        {
-            Ensure                          = 'Present'
-            Name                            = 'NET-Framework-45-Core'
-        }
+        #WindowsFeature DotNet45Core
+        #{
+        #    Ensure                          = 'Present'
+        #    Name                            = 'NET-Framework-45-Core'
+        #}
         WindowsFeature IIS
         {
             Ensure = 'Present'
             Name = 'Web-Server'
         }
-         WindowsFeature AspNet45
-        {
-            Ensure                          = "Present"
-            Name                            = "Web-Asp-Net45"
-        }
+         #WindowsFeature AspNet45
+        #{
+        #    Ensure                          = "Present"
+         #   Name                            = "Web-Asp-Net45"
+        #}
 
         # Configure Example.Web
         File Example.Web
@@ -134,61 +133,15 @@ Configuration NO.PowershellDSC.Example.Web
         {
             Ensure                          = "Present"
             Name                            = "Example.Web"
-            State                           = "Started"
-            autoStart                       = $true
-            CLRConfigFile                   = ''
-            enable32BitAppOnWin64           = $false
-            enableConfigurationOverride     = $true
-            managedPipelineMode             = 'Integrated'
-            managedRuntimeLoader            = 'webengine4.dll'
-            managedRuntimeVersion           = 'v4.0'
-            passAnonymousToken              = $true
-            startMode                       = 'OnDemand'
-            queueLength                     = 1000
-            cpuAction                       = 'NoAction'
-            cpuLimit                        = 90000
-            cpuResetInterval                = (New-TimeSpan -Minutes 5).ToString()
-            cpuSmpAffinitized               = $false
-            cpuSmpProcessorAffinityMask     = 4294967295
-            cpuSmpProcessorAffinityMask2    = 4294967295
-            identityType                    = 'SpecificUser'
-            idleTimeout                     = (New-TimeSpan -Minutes 20).ToString()
-            idleTimeoutAction               = 'Terminate'
-            loadUserProfile                 = $true
-            logEventOnProcessModel          = 'IdleTimeout'
-            logonType                       = 'LogonBatch'
-            manualGroupMembership           = $false
-            maxProcesses                    = 1
-            pingingEnabled                  = $true
-            pingInterval                    = (New-TimeSpan -Seconds 30).ToString()
-            pingResponseTime                = (New-TimeSpan -Seconds 90).ToString()
-            setProfileEnvironment           = $false
-            shutdownTimeLimit               = (New-TimeSpan -Seconds 90).ToString()
-            startupTimeLimit                = (New-TimeSpan -Seconds 90).ToString()
-            orphanActionExe                 = ''
-            orphanActionParams              = ''
-            orphanWorkerProcess             = $false
-            loadBalancerCapabilities        = 'HttpLevel'
-            rapidFailProtection             = $true
-            rapidFailProtectionInterval     = (New-TimeSpan -Minutes 5).ToString()
-            rapidFailProtectionMaxCrashes   = 5
-            autoShutdownExe                 = ''
-            autoShutdownParams              = ''
-            disallowOverlappingRotation     = $false
-            disallowRotationOnConfigChange  = $false
-            logEventOnRecycle               = 'Time,Requests,Schedule,Memory,IsapiUnhealthy,OnDemand,ConfigChange,PrivateMemory'
-            restartMemoryLimit              = 0
-            restartPrivateMemoryLimit       = 0
-            restartRequestsLimit            = 0
-            restartTimeLimit                = (New-TimeSpan -Minutes 1440).ToString()
-            restartSchedule                 = @('00:00:00', '08:00:00', '16:00:00')
+            State                           = "Stopped"
+            autoStart                       = $false
             DependsOn                       = "[WindowsFeature]IIS"
         }
         xWebsite Example.Web
         {
             Ensure = "Present"
             Name = "Example.Web"
-            State = "Started"
+            State = "Stopped"
             PhysicalPath = "C:\inetpub\Example.Web"
             BindingInfo = MSFT_xWebBindingInformation
             {
